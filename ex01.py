6e58488904d7a7baad4abb17300a91078c049dbe
Cathryne <katrinleinweber@MAC.local>
2014-08-09 19:20:27 +0200

    added comments

--- ex01.py
+++ ex01.py
@@ -1,7 +1,14 @@
+# Exercise 1: A Good First Program
+# http://learnpythonthehardway.org/book/ex1.html
+
 print "Hello World!"
 print "Hello Again"
 print "I like typing this."
 print "This is fun."
-print 'Yay! Printing.'
+# print statement makes Terminal display what's in "quotes"
+
+print 'Yay! Printing.' # 'single' quotes work as well
+
 print "I'd much rather you 'not'."
 print 'I "said" do not thouch this.'
+# alternation of "double" & 'single' quotes possible, if needed withing string
