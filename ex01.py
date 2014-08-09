9015c1198e7984f6c48959c54961b52c9d5a58d1
Cathryne <katrinleinweber@MAC.local>
2014-08-09 19:28:09 +0200

    Study Drills

--- ex01.py
+++ ex01.py
@@ -12,3 +12,12 @@ print 'Yay! Printing.' # 'single' quotes work as well
 print "I'd much rather you 'not'."
 print 'I "said" do not thouch this.'
 # alternation of "double" & 'single' quotes possible, if needed withing string
+
+# Study Drill 1: Make your script print another line.
+print "Another line..."
+
+# Study Drill 2: Make your script print only one of the lines.
+# Either remove excess lines or...
+
+# Study Drill 3: Put a '#' (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+# ...comment out
