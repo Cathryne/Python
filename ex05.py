678a9ff01e7f1f9aacf64ca94b86ea45018c7fb1
Cathryne <katrinleinweber@MAC.local>
2014-08-17 15:34:15 +0200

    added Study Drill 2

--- ex05.py
+++ ex05.py
@@ -24,4 +24,19 @@ print "His teeth are usually %s depending on the coffee." % teeth
 print "If I add %d, %d, and %d I get %d." % (
 	age, height, weight, age + height + weight)
 
+
 # Study Drill 1: removing all ""-parts from variable names
+
+
+# Study Drill 2: converting to imperial measurements via conversion factor as helper-variable
+inches_to_centimeters = 2.540 # https://duckduckgo.com/?q=inches+to+centimeters
+pounds_to_kilograms = 0.454 # https://duckduckgo.com/?q=pounds+to+kilograms
+
+print "%s's height in centimeter would be %d, or %r meters." % (
+	name, inches_to_centimeters * height,
+	round(inches_to_centimeters * height / 100, 2) # rounding to 2 decimal places
+	) # replace single space after comma in format sequence with new line better readability
+# different format characters can be used in same string: %d for integers, %r for decimals
+
+print "And his weight in kilograms: %r." % (pounds_to_kilograms * weight)
+# format sequence math needs to be enclosed by parentheses or commas
