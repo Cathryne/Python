782f8f61a2ff3ca29433d4896f32907d1fbe37dc
Cathryne <katrinleinweber@MAC.local>
2014-08-17 17:33:56 +0200

    added comments to exercise 6

--- ex06.py
+++ ex06.py
@@ -1,3 +1,7 @@
+# Exercise 6: Strings and Text
+# http://learnpythonthehardway.org/book/ex6.html
+
+# variable definitions can have string formatters, too
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
@@ -6,15 +10,19 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 print x
 print y
 
+# raw formatting of string within single quotes 
 print "I said: %r" % x
 print "I also said: '%s'." % y
+# string formatting of string doesn't include ''
 
+# Boolean and string variable definitions
 hilarious = False
-joke_evaluation = "Isn't that joke so funn?! %r"
+joke_evaluation = "Isn't that joke so funn?! %r" # value not inserted immediately
 
-print joke_evaluation % hilarious
+print joke_evaluation % hilarious # value inserted here
 
 w = "This is the left side of... "
 e = "A string with a right side."
 
+# addition of strings
 print w + e
