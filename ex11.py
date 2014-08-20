0b7a8fe8b2f3f769be08cf5105f71558194558ad
Cathryne <katrinleinweber@MAC.local>
2014-08-20 20:28:24 +0200

    added comments & shorter input alternative

--- ex11.py
+++ ex11.py
@@ -1,8 +1,19 @@
+# Exercise 11: Asking Questions
+# http://learnpythonthehardway.org/book/ex11.html
+
+# request input from user by:
+# a) displaying instructions separately from input function
 print "How old are you?",
+# b) applying input function
 age = raw_input()
-print "How tall are you?",
-height = raw_input()
+
+# alternative
+height = raw_input("How tall are you? ")
+
 print "How much do you weigh?",
-weight = raw_input()te
+weight = raw_input()
+# raw => can be mixed types, like number with unit => integer with string
+
+# displaying inputs in raw type (as entered) with string formatting
 print "So, you're %r old, %r tall and %r heavy." % (
 	age, height, weight)
