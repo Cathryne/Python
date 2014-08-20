727d48bf6e648ac398de07423c1f5da0d60ab124
Cathryne <katrinleinweber@MAC.local>
2014-08-20 20:43:56 +0200

    added input conversion to integers & floats, plus BMI calculation

--- ex11.py
+++ ex11.py
@@ -3,17 +3,26 @@
 
 # request input from user by:
 # a) displaying instructions separately from input function
-print "How old are you?",
+print "How many years are you old?",
 # b) applying input function
-age = raw_input()
+age = int(raw_input())
+# int() converts input to integer type
+
+# alternative, one-line input prompt
+height_m = float(raw_input("How tall are you (in m)? "))
+# float() converts input to loating point decimal type
 
 # alternative
-height = raw_input("How tall are you? ")
+# height_cm = int(raw_input("How tall are you (in m)? "))
+# height_m = float(float(height_cm) / 100)
 
-print "How much do you weigh?",
-weight = raw_input()
-# raw => can be mixed types, like number with unit => integer with string
+print "How much do you weigh (in kg)?",
+weight = int(raw_input())
 
 # displaying inputs in raw type (as entered) with string formatting
-print "So, you're %r old, %r tall and %r heavy." % (
-	age, height, weight)
+print "So, you're %d old, %d m tall and %d kg heavy." % (
+	age, height_m, weight)
+
+# calculate & display BMI, needs defined units
+BMI = weight / ( height_m * height_m )
+print "Therefore, your BMI is: %d." % BMI
