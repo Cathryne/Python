a7804b6306210648e390d234ac129d72c668f6c3
Cathryne <katrinleinweber@MAC.local>
2014-09-10 20:44:11 +0200

    added command argument conversion, type checks and exception handling

--- ex13.py
+++ ex13.py
@@ -12,4 +12,35 @@ script, first, second, third = argv
 print "The script is called:", script 
 print "Your 1st variable is:", first 
 print "Your 2nd variable is:", second
-print "Your 3rd variable is:", third 
+print "Your 3rd variable is:", third
+
+
+# Common Student Question: Are the command line arguments strings?
+
+print "Any numbers here?"
+print type(first)
+print type(second)
+print type(third)
+# type() returns type of variable; will always be 'str' for command arguments
+
+print "I guess not. Yet."
+print "Processing... Done!"
+
+# exception handling for "ValueError: could not convert string to float"
+try:
+	first = float(first)
+	print "Your 1st variable is the number:", first
+except ValueError:
+	print "Your 1st variable is not a number."
+	
+try:	
+	second = float(second)
+	print "Your 2nd variable is the number:", second
+except ValueError:
+	print "Your 2nd variable is not a number."
+	
+try:
+	third = float(third)
+	print "Your 3rd variable is the number:", third
+except ValueError:
+	print "Your 3rd variable is not a number."
