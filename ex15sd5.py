b1c1c5f021b1966cab3854256f7323a32c826811
Cathryne <katrinleinweber@MAC.local>
2014-09-15 21:08:30 +0200

    converted argv from ex15 to raw_input (Study Drill 5)

---
+++ ex15sd5.py
@@ -0,0 +1,15 @@
+# Exercise 15: Reading Files
+# http://learnpythonthehardway.org/book/ex15.html
+# Study Drill 5: Use raw_input() instead of argv
+
+# get filename from user
+filename = raw_input("\nWhich file do you want to open? Please type its name here, including file extension: ")
+
+# opens file and assigned it to variable
+txt = open(filename)
+# print type(txt) would return 'file'
+
+# displayes filename (given as command argument)...
+print "OK, thanks! Here's your file %r:" % filename
+print txt.read()    #  ...and file contents after calling read function on file variable
+txt.close()
