beb45fd88bc9924b5424ce524ab9b3d0e796065d
Cathryne <katrinleinweber@MAC.local>
2014-09-15 21:02:51 +0200

    commented & corrected ex15; added Study Drill 7

--- ex15.py
+++ ex15.py
@@ -1,15 +1,27 @@
+# Exercise 15: Reading Files
+# http://learnpythonthehardway.org/book/ex15.html
+
+# loads sys and argv modules
 from sys import argv
 
+# unpacks command arguments into variables
 script, filename = argv
 
+# opens file and assigned it to variable
 txt = open(filename)
+# print type(txt) would return 'file'
 
+# displayes filename (given as command argument)...
 print "Here's your file %r:" % filename
-print txt.read()
+print txt.read()    #  ...and file contents after calling read function on file variable
 
+# let user confirm filename; open and display again
 print "Type the filename again:"
 file_again = raw_input("> ")
-
-tyt_again = open(file_again)
-
+txt_again = open(file_again)
 print txt_again.read()
+
+
+# Study Drill 7: properly close files by calling close() method on file variable
+txt_again.close()
+txt.close()
