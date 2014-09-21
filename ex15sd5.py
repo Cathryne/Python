80f15bbd4649e4e63a9473b7fda7521baaf9529a
Cathryne <katrinleinweber@MAC.local>
2014-09-21 20:36:55 +0200

    initial commit of ex16

--- ex15sd5.py
+++ ex15sd5.py
@@ -5,18 +5,18 @@
 # get filename from user
 filename = raw_input("\nWhich file do you want to open? Please type its name here, including file extension: ")
 
-# opens file and assigned it to variable
+# open file and assigned it to variable
 file_object = open(filename)
 # print type(txt) would return 'file'
 
-# displayes filename (given as command argument)...
+# display filename (given as command argument)...
 print "OK, thanks! Here's your file %r:" % filename
 
-# gets content from file...
+# get content from file...
 file_content = file_object.read()
 
-# ...and displays that
+# ...and display that
 print file_content
 
-# closes file
+# close file
 file_object.close()
