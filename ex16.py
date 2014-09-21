254fa92597bc5e5de44eedeb77e3fe9608ea1fca
Cathryne <katrinleinweber@MAC.local>
2014-09-21 21:02:52 +0200

    commented ex16 & added Study Drills 3-5

--- ex16.py
+++ ex16.py
@@ -1,33 +1,35 @@
-from sys import argv
+# Exercise 16: Reading and Writing Files
+# http://learnpythonthehardway.org/book/ex16.html
 
+# pass & unpack command argument into script
+from sys import argv
 script, filename = argv
 
+# prompt user to abort or confirm file overwrite
 print "We're going to erase %r." % filename
 print "If you don't want that, press CTRL+V (^C)."
 print "If you do want that file erased, press RETURN."
-
 raw_input("? ")
 
-print "Openeing the file..."
+print "Opening the file..."
+# open file in write mode
 target = open(filename, 'w')
+# Study Drill 4: open() defaults to read-only mode
 
+# Overwrite content by truncating file to 0 bytes
 print "Truncating the file. Good bye!"
 target.truncate()
+# Study Drill 5: Not required, because opening in write mode already overwrites existing content. Tested by by adding target.flush() and checking file size & content changes while script ran.
 
 print "Now I'm going to ask you for three lines to write into the file."
-
+# Prompt user for file contents
 line1 = raw_input("line 1: ")
 line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
-
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# Study Drill 3: simplify multiple write commands with multi-line
+target.write("%s\n%s\n%s" % (line1, line2, line3))
 
 print "And finally, we close the file..."
 target.close()
