141f4f079a70fb716db8d82a6fef58f47c546234
Cathryne <katrinleinweber@MAC.local>
2014-09-24 20:41:51 +0200

    initial commit of ex18

--- ex18.py
+++ ex18.py
@@ -4,3 +4,18 @@ def print_two(*args):
 	print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # OK, that *args is actually pointless. We Can just do this
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# this just takes 1 argument
+def print_one(arg1):
+	print "arg1: %r" % arg1
+
+# this one takes no arguments
+def print_none():
+	print "I got nothin'."
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
