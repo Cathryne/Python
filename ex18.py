cc8259038c2c09f1a0078dda9b676143d2aa5f76
Cathryne <katrinleinweber@MAC.local>
2014-09-24 21:09:02 +0200

    commented ex18

--- ex18.py
+++ ex18.py
@@ -1,9 +1,12 @@
-# this one is like your scripts with argv
+# Exercise 18: Names, Variables, Code, Functions
+# http://learnpythonthehardway.org/book/ex18.html
+
+# function definition with argument unpacking like with argv 
 def print_two(*args):
 	arg1, arg2 = args
 	print "arg1: %r, arg2: %r" % (arg1, arg2)
 
-# OK, that *args is actually pointless. We Can just do this
+# unpacking kinda pointless, write arguments out for clarity
 def print_two_again(arg1, arg2):
 	print "arg1: %r, arg2: %r" % (arg1, arg2)
 
@@ -15,6 +18,7 @@ def print_one(arg1):
 def print_none():
 	print "I got nothin'."
 
+# function calls with arguments (in number and order as defined above)
 print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
 print_one("First!")
