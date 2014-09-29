15ef6ba565c9e57c821b410b3a6bd53ad9b00f6f
Cathryne <katrinleinweber@MAC.local>
2014-09-29 20:51:27 +0200

    completed ex21 comments and Study Drills

--- ex21.py
+++ ex21.py
@@ -1,6 +1,9 @@
+# Exercise 21: Functions Can Return Something
+# http://learnpythonthehardway.org/book/ex21.html
+
 def add(a, b):
 	print "ADDING %d + %d" % (a, b)
-	return a + b
+	return a + b	# function call receives the value stated here 
 
 def subtract(a, b):
 	print "SUBTRACTING %d - %d" % (a, b)
@@ -17,6 +20,8 @@ def divide(a, b):
 
 print "Let's do some math with just functions!"
 
+# variable definitions based on return values from functions
+# functions calls pass values into function; return statement passes values out again
 age = add(30, 5)
 height = subtract(78, 4)
 weight = multiply(90, 2)
@@ -28,6 +33,10 @@ print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 # A puzzle for the extra credit
 print "Here's a puzzle."
 
-what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+# nested function calls are executed inside out
+# Study Drills 2 & 3: unwrap function calls to formula with variables
+what1 = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+what2 = 30 + 5 + (78 - 4 - (90 * 2 * (100 / 2 / 2)))
 
-print "That becomes: ", what, "Can you do it by hand?"
+print "The function-based formula says:", what1
+print "The hand-made formula says:", what2
