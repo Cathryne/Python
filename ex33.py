0a2e79e4d3dd1f78e818c2fd84bedf497e27b750
Cathryne <Cathryne@users.noreply.github.com>
2014-10-05 20:33:35 +0200

    commented & optimised ex33

--- ex33.py
+++ ex33.py
@@ -1,16 +1,24 @@
-i = 0
-numbers = []
+# Exercise 33: While Loops
+# http://learnpythonthehardway.org/book/ex33.html
 
+i = 0   # initialise variable
+numbers = []    # initialise empty list
+
+# initialise while-loop with iterator
+# executes indented code block until boolean expression returns FALSE
 while i < 6:
-    print "At the top i is %d." % i
+    print "i at the top: %d" % i
+
+    # iterative action
     numbers.append(i)
 
-    i = i + 1
-    print "Numbers now:", numbers
-    print "At the bottom i is %d." % i
+    # incrementation of iterator absolutely essential to prevent infinite looping
+    i += 1
 
+    # useful checks for status of iterator & result of iterative action
+    print "Numbers now:", numbers
+    print "i at the bottom: %d" % i
 
 print "The numbers:"
-
 for num in numbers:
     print num
