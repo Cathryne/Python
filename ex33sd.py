369c497bfc59b84e7c9596e40dc63365ac9419b9
Cathryne <Cathryne@users.noreply.github.com>
2014-10-05 21:46:22 +0200

    repeated ex33 Study Drill 1

--- ex33sd.py
+++ ex33sd.py
@@ -1,16 +1,21 @@
 # Exercise 33: While Loops
 # http://learnpythonthehardway.org/book/ex33.html
 
+# functionalised while-loop
+def iterate(i):
+    while i < 6:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
 i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+#function call passes iterator
+iterate(i)
 
 
 print "The numbers: "
