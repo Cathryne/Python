a36c0bf508d0b93fd7bb1b8839606148d105fee8
Cathryne <Cathryne@users.noreply.github.com>
2014-10-05 20:44:26 +0200

    converted while- to functionalised for-loop, buggy though
    range(N) doesn’t respect step-size

--- ex33.py
+++ ex33.py
@@ -1,23 +1,26 @@
 # Exercise 33: While Loops
 # http://learnpythonthehardway.org/book/ex33.html
 
-i = 0   # initialise variable
+# functionalised loop
+def iterate(N, M):
+    for i in range(N):
+        print "i at the top: %d" % i
+        numbers.append(i)   # iterative action
+        i += M  # incrementation of iterator absolutely essential to prevent infinite looping
+        # useful checks for status of iterator & result of iterative action
+        print "Numbers now:", numbers
+        print "i at the bottom: %d" % i
+
+
+N = int(raw_input("Please provide an end number (start is 0): "))
+M = int(raw_input("And a step-size, please: "))
+
 numbers = []    # initialise empty list
+
+iterate(N, M)
 
 # initialise while-loop with iterator
 # executes indented code block until boolean expression returns FALSE
-while i < 6:
-    print "i at the top: %d" % i
-
-    # iterative action
-    numbers.append(i)
-
-    # incrementation of iterator absolutely essential to prevent infinite looping
-    i += 1
-
-    # useful checks for status of iterator & result of iterative action
-    print "Numbers now:", numbers
-    print "i at the bottom: %d" % i
 
 print "The numbers:"
 for num in numbers:
