f70ce6942f1bb6221a02bac04d16092eedc9d0aa
Cathryne <Cathryne@users.noreply.github.com>
2014-10-07 21:10:07 +0200

    started ex34: Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,13 @@
+# Exercise 34: Accessing Elements of Lists
+# http://learnpythonthehardway.org/book/ex34.html
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at 1 is the %s." % animals[1]
+print "The third (3rd) animal is the %s at list index %d." % (animals[2], 2 - 1)
+print "The first (1st) animal is the %s at list index %d." % (animals[0], 1 - 1)
+print "The animal at 3 is the %s." % animals[3]
+print "The fifth (5th) animal is the %s at list index %d." % (animals[4], 5 - 1)
+print "The animal at 2 is the %s." % animals[2]
+print "The sixth (6th) animal is the %s at list index %d." % (animals[5], 6 - 5)
+print "The animal at 4 is the %s." % animals[4]
