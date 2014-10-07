712b86e4300382b22d726ac8b380650c863c59cc
Cathryne <Cathryne@users.noreply.github.com>
2014-10-07 21:42:09 +0200

    commented & continued ex34

--- ex34.py
+++ ex34.py
@@ -1,13 +1,36 @@
 # Exercise 34: Accessing Elements of Lists
 # http://learnpythonthehardway.org/book/ex34.html
 
-animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+def get_animal_out(animal_out, animals):
+    if animal_out == "exit":
+        print "OK, exiting..."
+    else:
+        print "OK, at index %d I found the %s." % (animal_out, animals[animal_out])
 
-print "The animal at 1 is the %s." % animals[1]
-print "The third (3rd) animal is the %s at list index %d." % (animals[2], 2 - 1)
-print "The first (1st) animal is the %s at list index %d." % (animals[0], 1 - 1)
-print "The animal at 3 is the %s." % animals[3]
-print "The fifth (5th) animal is the %s at list index %d." % (animals[4], 5 - 1)
-print "The animal at 2 is the %s." % animals[2]
-print "The sixth (6th) animal is the %s at list index %d." % (animals[5], 6 - 5)
-print "The animal at 4 is the %s." % animals[4]
+# prompt user for 1st element & initialise list with that
+animals = []
+animal_in = str(raw_input("Which animal shall I add to the list? "))
+
+# keep prompting for more elements
+print "A few more, please. type 'exit' to close."
+while animal_in != "exit":
+    animals.append(animal_in) # can take 1st animal_in from before loop, but doesn't take last "exit"
+    animal_in = str(raw_input("Which animal shall I add to the list? "))
+    if animal_in == "exit":
+		print "OK, exiting..."
+
+print "\nOK, the list is now %d elements long: %r" % (len(animals), animals)
+
+# set initial value to max. index
+animal_out = len(animals) - 1
+print "And it ends with the %s." % animals[animal_out]
+
+while animal_out < len(animals):
+    animal_out = int(raw_input("\nA few more? Again, type 'exit' to stop. "))
+    if animal_out >= len(animals):
+        print "Sorry, the maximum index is only %d. Exiting..." % (len(animals) - 1)
+    else:
+        get_animal_out(animal_out, animals)
+
+
+
