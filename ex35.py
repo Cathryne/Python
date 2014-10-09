da3b409fb1db4bd86f9db5c490c4da627dc601b7
Cathryne <Cathryne@users.noreply.github.com>
2014-10-09 20:35:33 +0200

    comments and better number conversion for ex35

--- ex35.py
+++ ex35.py
@@ -1,19 +1,25 @@
+# Exercise 35: Branches and Functions
+# http://learnpythonthehardway.org/book/ex35.html
+
+# make exit module/class available
 from sys import exit
 
+# function for gold room: ask user for number input & react depending on number value
 def gold_room():
-    print "This room is full of gold. How much do you take?"
+	choice = raw_input("This room is full of gold. How much do you take? ")
+	# other variant of checking input for integer type:  if next.isdigit(), see  http://stackoverflow.com/a/5424750
+	try:
+		val = int(choice)
+		how_much = int(choice)
+	except ValueError:
+	    dead("Man, learn to type a number.") # jumps to line 63
 
-    choice = raw_input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
-    else:
-        dead("Man, learn to type a number!")
-
-    if how_much < 50:
-        print "Nice, you're not greedy, you win!"
-        exit(0)
-    else:
-        dead("You greedy bastard!")
+	if how_much < 50:
+		print "Nice! Not greedy wins!"
+		exit(0) # exits program gracefully (0 means no error, anything else would be an error message ID number)
+	else:
+		print "The bear hears you rummaging about and drags you to his room."
+		bear_room() # jumps to line 63
 
 
 def bear_room():
@@ -23,18 +29,18 @@ def bear_room():
     print "How are you going to move the bear?"
     bear_moved = False
 
-    while True:
+    while True: # infinite loop => needs dedicated exit point(s; 2 stages here) somewhere in it, because while statement can't become False
         choice = raw_input("> ")
 
         if choice == "take honey":
-            dead("The bear looks at you then slaps your face off.")
+            dead("The bear looks at you then slaps your face off.") # jumps to line 63
         elif choice == "taunt bear":
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
         elif choice == "taunt bear" and "bear moved":
-            dead("The bear gets pissed off and chews your leg off.")
+            dead("The bear gets pissed off and chews your leg off.") # jumps to line 63
         elif choice == "open door" and bear_moved:
-            gold_room()
+            gold_room() # jumps to line 8
         else:
             print "I got no idea what that means."
 
@@ -47,11 +53,11 @@ def cthulhu_room():
     choice = raw_input("> ")
 
     if "flee" in choice:
-        start()
+        start() # jumps to line 87
     elif "head" in choice:
-        dead("Well that was tasty!")
+        dead("Well that was tasty!") # jumps to line 63
     else:
-        cthulhu_room()
+        cthulhu_room() # jumps to line 48
 
 
 def dead(why):
@@ -66,11 +72,14 @@ def start():
     choice = raw_input("> ")
 
     if choice == "left":
-        bear_room()
+        bear_room() # jumps to line 25
     elif choice == "right":
-        cthulhu_room()
+        cthulhu_room() # jumps to line 48
+    elif choice == "front":
+        print "Congratulations! You found a short-cut!"
+        gold_room() # jumps to line 8
     else:
         dead("You stumble around the room until you starve.")
 
 
-start()
+start() # jumps to line 67
