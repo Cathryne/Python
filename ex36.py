b0740f8d918d7e94774e425cea671d2fc42f768d
Cathryne <Cathryne@users.noreply.github.com>
2014-10-12 16:57:21 +0200

    tried to add looped check for partial room match

--- ex36.py
+++ ex36.py
@@ -18,7 +18,6 @@ def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
-
 def do_stuff(action, choice1, choice2, result1, result2):
     choice = str(raw_input(action))
     if not ((choice1 in choice) or (choice2 in choice)):
@@ -36,20 +35,34 @@ def do_stuff(action, choice1, choice2, result1, result2):
 # Room selection
 
 print """You stand in the hallway of CleanLab.
-There are the following rooms:"""
-rooms = ["protein lab", "microscopy room"] # , "DNA lab", "climate chamber", "freezer room", "PhD office", "PostDoc office", "PI office", "storage rooom"
+You can enter one of the these rooms:"""
+rooms = ["protein lab", "DNA lab", "microscopy room"] # , "climate chamber", "freezer room", "PhD office", "PostDoc office", "PI office", "storage rooom"
 
 for i in rooms:
     print "\t", i
     i += i
 
-enter = raw_input("Which room do enter? ")
 
-if "micro" in enter:
+# def room_choice():
+#     room = str(raw_input("Which room do you want to enter? "))
+#     if any(room in entry for entry in rooms) == True: # learned from http://stackoverflow.com/a/4843172
+#         matching = (s for s in rooms if room in s)
+#         return matching
+#     else:
+#         print "Try again."
+#         room_choice()
+
+room = str(raw_input("Which room do you want to enter? "))
+
+
+if "micro" in room:
     print "\nIt's dark in the microscopy room. You flick the light switch and...\n...find a dysfunctional UV emitter. What do you do to fix it?"
     do_stuff("Tune emission or replace light source? ", "une", "eplace", "That was a quick and easy solution. In the short term.", "You spent a lot of money, but the microscope will work for a long time now!")
-elif "protein" in enter:
+elif "protein" in room:
     print "\nYou hear the squeaking of an old gel shaker.\nWhat do you do to stop the squeaking?"
     do_stuff("Lubricate with some oil or reduce shaking speed? ", "ubricate", "educe", "Good job! The gels will be well shaken and the blots will work nicely!", "Useless! The gels will be inhomogeneous and the blots won't work well.")
+elif "DNA" in room:
+    print "\nYou see the thermocycler's hazard light blinking.\nThe cooling block broke at the end of the sampling run. What do you do with the PCR products inside?"
+    do_stuff("Leave there or transfer to fridge? ", "leave", "transfer", "Not a good idea :-( The samples degraded and your colleague has to repeat the experiment.", "Thanks for saving the samples :-) Just don't forget to tell your colleague where exactly you put the samples.")
 else:
     restart_program()
