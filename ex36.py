d354b4c69a0c63ca4ec0247cec44976ff4a9110f
Cathryne <Cathryne@users.noreply.github.com>
2014-10-12 13:53:40 +0200

    do_stuff() loops properly now

--- ex36.py
+++ ex36.py
@@ -20,9 +20,11 @@ def restart_program():
 
 
 def do_stuff(action, choice1, choice2, result1, result2):
-    print  "What do you do?"
     choice = str(raw_input(action))
-    if choice1 in choice:
+    if not ((choice1 in choice) or (choice2 in choice)):
+        print "Try again."
+        do_stuff(action, choice1, choice2, result1, result2) # learned from https://stackoverflow.com/questions/12556907/continually-prompting-user-for-input-in-python
+    elif choice1 in choice:
         print "\t", result1
     elif choice2 in choice:
         print "\t", result2
@@ -35,7 +37,7 @@ def do_stuff(action, choice1, choice2, result1, result2):
 
 print """You stand in the hallway of CleanLab.
 There are the following rooms:"""
-rooms = ["protein lab", "DNA lab", "climate chamber", "freezer room", "PhD office", "PostDoc office", "PI office", "storage rooom", "microscopy room"]
+rooms = ["protein lab", "microscopy room"] # , "DNA lab", "climate chamber", "freezer room", "PhD office", "PostDoc office", "PI office", "storage rooom"
 
 for i in rooms:
     print "\t", i
@@ -44,11 +46,10 @@ for i in rooms:
 enter = raw_input("Which room do enter? ")
 
 if "micro" in enter:
-    print """\nIt's dark in the microscopy room. You flick the light switch and...
-    ...find an old UV emitter."""
-    do_stuff("Tune emission or replace light source? ", "une", "eplace", "That wasa quick and easy solution. In the short term.", "You spent a lot of money, but the microscope will work for a long time now!")
+    print "\nIt's dark in the microscopy room. You flick the light switch and...\n...find a dysfunctional UV emitter. What do you do to fix it?"
+    do_stuff("Tune emission or replace light source? ", "une", "eplace", "That was a quick and easy solution. In the short term.", "You spent a lot of money, but the microscope will work for a long time now!")
 elif "protein" in enter:
-    print "\nYou hear the squeaking of an old gel shaker."
+    print "\nYou hear the squeaking of an old gel shaker.\nWhat do you do to stop the squeaking?"
     do_stuff("Lubricate with some oil or reduce shaking speed? ", "ubricate", "educe", "Good job! The gels will be well shaken and the blots will work nicely!", "Useless! The gels will be inhomogeneous and the blots won't work well.")
 else:
     restart_program()
