e2bfa911927f3eca78384a834f47553eaabb188c
Cathryne <Cathryne@users.noreply.github.com>
2014-10-12 13:45:11 +0200

    completed 2 rooms

--- ex36.py
+++ ex36.py
@@ -1,29 +1,54 @@
 # Exercise 36: Designing and Debugging (own game: CleanLab)
 # http://learnpythonthehardway.org/book/ex36.html
 
+print """\nWelcome to CleanLab! The cleanest laboratory at this university!
+If anyone bothers. Which you do, right...?\n
+To progress in this game, type room names
+and verbs from the choices presented to you."""
+
 # enables restarting of program
 import sys
 import os
+
+
 def restart_program():
     """Restarts the current program, without returning anything before-hand
     Hence, clean-up and save data before calling."""
+    raw_input("I have no idea what you mean!?! Press any key to restart... ")
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
-print "\nWelcome to CleanLab! The cleanest laboratory at this university! If anyone bothers."
-print "To progress in this game, type room names and verbs from the choices presented to you."
 
-def find_old_shit(object, action, choice1, choice2, result1, result2):
-    print "\nYou found an old %s. What do you do?." % object
+def do_stuff(action, choice1, choice2, result1, result2):
+    print  "What do you do?"
     choice = str(raw_input(action))
     if choice1 in choice:
-        print result1
+        print "\t", result1
     elif choice2 in choice:
-        print result2
+        print "\t", result2
     else:
-        raw_input("I have no idea what you mean!?! Press any key to restart... ")
         restart_program()
 
-print "You stand in the hallway from which the following"
 
-find_old_shit("Microscope", "Polish lenses or tune light source? ", "polish", "tune", "Focussing works again!", "Samples are visible again!")
+
+# Room selection
+
+print """You stand in the hallway of CleanLab.
+There are the following rooms:"""
+rooms = ["protein lab", "DNA lab", "climate chamber", "freezer room", "PhD office", "PostDoc office", "PI office", "storage rooom", "microscopy room"]
+
+for i in rooms:
+    print "\t", i
+    i += i
+
+enter = raw_input("Which room do enter? ")
+
+if "micro" in enter:
+    print """\nIt's dark in the microscopy room. You flick the light switch and...
+    ...find an old UV emitter."""
+    do_stuff("Tune emission or replace light source? ", "une", "eplace", "That wasa quick and easy solution. In the short term.", "You spent a lot of money, but the microscope will work for a long time now!")
+elif "protein" in enter:
+    print "\nYou hear the squeaking of an old gel shaker."
+    do_stuff("Lubricate with some oil or reduce shaking speed? ", "ubricate", "educe", "Good job! The gels will be well shaken and the blots will work nicely!", "Useless! The gels will be inhomogeneous and the blots won't work well.")
+else:
+    restart_program()
