6e3e27d4db9ff490ba43ff69f5a56cc38371983b
Cathryne <Cathryne@users.noreply.github.com>
2014-10-28 20:28:31 +0100

    basic hallway code; needs functionalisation of room actions

--- ex36.py
+++ ex36.py
@@ -19,6 +19,7 @@ def restart(message):
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
+
 def do_stuff(action, choice1, choice2, result1, result2):
     choice = str(raw_input(action))
     if not ((choice1 in choice) or (choice2 in choice) or "leave"):
@@ -52,17 +53,17 @@ def option_choice(N_options, message):
     else:
         return option_number
 
-#     if any(room in entry for entry in rooms) == True: # learned from http://stackoverflow.com/a/4843172
-#         matching = (s for s in rooms if room in s)
-#         return matching
-#     else:
-#         print "Try again."
-#         option_choice()
 
 def list_options(list_name):
     for i in list_name:
         print "\t%d:" % (list_name.index(i) + 1), i
         i += i
+
+
+def back_to_hallway(room_exit):
+    raw_input("Press any key to return to the hallway ")
+    print "You exited the %s" % room_exit
+    room = option_choice(N_rooms, "Which room do you want to enter now? ")
 
 
 print """You stand in the hallway of CleanLab.
@@ -75,12 +76,15 @@ room = option_choice(N_rooms, "Which room do you want to enter? Please type its 
 if room == 1:
     print "\nIt's dark in the microscopy room. You flick the light switch and...\n...find a dysfunctional UV emitter. What do you do to fix it?"
     do_stuff("Tune emission or replace light source? ", "une", "eplace", "That was a quick and easy solution. In the short term.", "You spent a lot of money, but the microscope will work for a long time now!")
+    back_to_hallway(rooms[0])
 elif room == 2:
     print "\nYou hear the squeaking of an old gel shaker.\nWhat do you do to stop the squeaking?"
     do_stuff("Lubricate with some oil or reduce shaking speed? ", "ubricate", "educe", "Good job! The gels will be well shaken and the blots will work nicely!", "Useless! The gels will be inhomogeneous and the blots won't work well.")
+    back_to_hallway(rooms[1])
 elif room == 3:
     print "\nYou see the thermocycler's hazard light blinking.\nThe cooling block broke at the end of the sampling run. What do you do with the PCR products inside?"
     do_stuff("Let it be or transfer to fridge? ", "et", "ransfer", "Not a good idea :-( The samples degraded and your colleague has to repeat the experiment.", "Thanks for saving the samples :-) Just don't forget to tell your colleague where exactly you put the samples.")
+    back_to_hallway(rooms[2])
 elif room == 4:
     print "\nThere's lots of chatter in the office."
     persons = ["Professor", "PostDoc", "Technician"]
@@ -90,11 +94,17 @@ elif room == 4:
 
     if person == 1:
         print "\tNo time, sorry. Have to prepare talk."
+        back_to_hallway(rooms[3])
     elif person == 2:
         print "\tNo time, sorry. Have to write manuscript."
+        back_to_hallway(rooms[3])
     elif person == 3:
         print "\tNo time, sorry. Have to prepare experiment."
+        back_to_hallway(rooms[3])
     else:
-        restart("OK, back to the hallway.")
+        back_to_hallway(rooms[3])
+
+    back_to_hallway(rooms[3])
+
 else:
     restart("That was not any of those numbers!")
