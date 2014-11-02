8ce21692651b4660a31a4e9a3de55b7179257b2e
Cathryne <Cathryne@users.noreply.github.com>
2014-11-02 10:44:23 +0100

    continued ex39 & renamed some files

---
+++ ex39c.py
@@ -0,0 +1,58 @@
+# Exercise 39: Dictionaries, Oh Lovely Dictionaries
+# http://learnpythonthehardway.org/book/ex39.html
+
+import ex39c_hashmap
+
+# create a mapping of state to abbreviation
+states = ex39c_hashmap.new()
+ex39c_hashmap.set(states, 'Oregon', 'OR')
+ex39c_hashmap.set(states, 'Florida', 'FL')
+ex39c_hashmap.set(states, 'California', 'CA')
+ex39c_hashmap.set(states, 'New York', 'NY')
+ex39c_hashmap.set(states, 'Michigan', 'MI')
+
+# create a basic set of states and some cities in them
+cities = ex39c_hashmap.new()
+ex39c_hashmap.set(cities, 'CA', 'San Francisco')
+ex39c_hashmap.set(cities, 'MI', 'Detroit')
+ex39c_hashmap.set(cities, 'FL', 'Jacksonville')
+
+# add some more cities
+ex39c_hashmap.set(cities, 'NY', 'New York')
+ex39c_hashmap.set(cities, 'OR', 'Portland')
+
+
+# print out some cities
+print '-' * 10
+print "NY State has: %s" % ex39c_hashmap.get(cities, 'NY')
+print "OR State has: %s" % ex39c_hashmap.get(cities, 'OR')
+
+# print some states
+print '-' * 10
+print "Michigan's abbreviation is: %s" % ex39c_hashmap.get(states, 'Michigan')
+print "Florida's abbreviation is: %s" % ex39c_hashmap.get(states, 'Florida')
+
+# do it by using the state then cities dict
+print '-' * 10
+print "Michigan has: %s" % ex39c_hashmap.get(cities, ex39c_hashmap.get(states, 'Michigan'))
+print "Florida has: %s" % ex39c_hashmap.get(cities, ex39c_hashmap.get(states, 'Florida'))
+
+# print every state abbreviation
+print '-' * 10
+ex39c_hashmap.list(states)
+
+# print every city in state
+print '-' * 10
+ex39c_hashmap.list(cities)
+
+print '-' * 10
+# by default ruby says "nil" when something isn't in there
+state = ex39c_hashmap.get(states, 'Texas')
+
+if not state:
+  print "Sorry, no Texas."
+
+# default values using ||= with the nil result
+# can you do this on one line?
+city = ex39c_hashmap.get(cities, 'TX', 'Does Not Exist')
+print "The city for the state 'TX' is: %s" % city
