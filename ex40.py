f7d0a2ac65dfe7d8aab7f8e7e7f5f344cb506be1
Cathryne <Cathryne@users.noreply.github.com>
2014-11-03 19:41:09 +0100

    added Study Drills 1 & 2 to ex40

--- ex40.py
+++ ex40.py
@@ -5,14 +5,15 @@
 class Song(object):
 
     # initialises empty object upon instantiation of class
-    def __init__(self, lyrics):
+    def __init__(self, input):
         # assignment of variable that is passed into class; self specifies instance attribute & differentiates from potential local variable of same name
-        self.lyrics = lyrics
+        self.lyrics = input
 
     # custom function definition that iterates through lines of list variable
     def sing_me_a_song(self):
-        for line in self.lyrics:
-            print line
+        print "-" * 10
+        for i in self.lyrics:  # "line" was only iterator & can therefore be renamed
+            print i
 
 # instantiates classes & passes lyric lists into resulting objects
 happy_bday = Song(["Happy birthday to you",
@@ -20,7 +21,16 @@ happy_bday = Song(["Happy birthday to you",
                    "So I'll stop right there"])
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells"])
+# Study Drill 1:  Write some more lists of strings
+humming = Song(["Dadaa Dada Dadadada",
+                "Dadum Dadum Dadadadum"])
+
+# Study Drill 2: Put lyrics in separate variable & pass into class
+bird_song = ["Chirp", "Chirp", "Chirp"]
+bird = Song(bird_song)
 
 # calls custom function from class on objects that contain lyric lists
 happy_bday.sing_me_a_song()
 bulls_on_parade.sing_me_a_song()
+humming.sing_me_a_song()
+bird.sing_me_a_song()
